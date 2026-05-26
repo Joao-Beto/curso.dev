c823b97ca8f5fc53e72bdcc9d54a4bee8bb3797e
João Gilberto <beto@imaginarte.com.br>
2026-05-26 18:50:36 +0000

    adiciona arquivo .prettierignore

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,4 @@ function Home() {
   return <h1>Fala, Camarada! yeh,yeh!</h1>;
 }
 
-function teste() {
-  console.log("teste");
-}
-
-function teste2() {
-  console.log("identação errada");
-}
-
 export default Home;
